d5c29fdb698455c67d138d5c83e183d8dfb888c3
BRIPOT <b.potosi@syncpoint.info>
2025-12-13 22:06:49 +0100

    dfdfd

--- angular-client/src/app/app.ts
+++ angular-client/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ImageGenerator } from './image-generator/image-generator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ImageGenerator],
+  imports: [ImageGenerator],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
